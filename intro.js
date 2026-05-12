8e7aa9da505713a8153f6e0078e3cc162af5df15
njile junior <s3n3t3@agnes.senete>
2026-05-12 21:04:10 +0300

    Intro to javascript

---
+++ intro.js
@@ -0,0 +1,12 @@
+const myName = "senete erick";
+const age = 23;
+const favProgrammingLanguage = "c++";
+
+console.log(
+  "hi my name is ",
+  myName,
+  ",i am ",
+  age,
+  "0ld. my favorite programming lanuage is",
+  favProgrammingLanguage,
+);
